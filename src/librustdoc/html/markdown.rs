4ec3ebbca69847ff02d20cd8a3ff89eeb08ff969
Alexis Beingessner <a.beingessner@gmail.com>
2014-09-04 21:03:34 -0400

    fixup

--- src/librustdoc/html/markdown.rs
+++ src/librustdoc/html/markdown.rs
@@ -80,7 +80,9 @@ struct hoedown_renderer {
                                     *mut libc::c_void)>,
     header: Option<extern "C" fn(*mut hoedown_buffer, *const hoedown_buffer,
                                  libc::c_int, *mut libc::c_void)>,
-    other: [libc::size_t, ..29],
+    math: Option<extern "C" fn(*mut hoedown_buffer, *const hoedown_buffer,
+                                 libc::c_int, *mut libc::c_void)>,
+    other: [libc::size_t, ..28],
 }
 
 #[repr(C)]
